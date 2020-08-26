892fbd8cac1e22b6bc7a87d671fab5bf6d51f434
rodrigo <rodrigomiliano16@gmail.com>
2020-08-26 00:48:35 -0300

    Ejercicio del 25/8 clase 2 resuelto.

---
+++ 20200825/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int intentos = 0;
+            //string clave = "";
+            //while (clave != "secreto" & intentos < 4)                            
+            //{
+            //    Console.Write("Ingrese la constrasena: ");
+            //    clave = Console.ReadLine();
+            //    intentos++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(numero);
+            //} while (numero < 10);
+
+
+            //Console.WriteLine("Hola Mundo!");
+
+
+           
+            int numeroSecreto = 8;
+            int numeroApostado = 0;
+
+            while (numeroSecreto != numeroApostado)
+            {
+                Console.WriteLine("Adivina el numero: elige entre 0 y 9");
+                numeroApostado = int.Parse(Console.ReadLine());                
+                if (numeroSecreto != numeroApostado)
+                {
+                    Console.Write("Incorrecto, segui intentando.");                    
+                }
+                else
+                {
+                    Console.WriteLine("Ganaste: el numero era " + numeroSecreto);
+                }
+
+            }
+            
+
+
+        }
+    }
+}
