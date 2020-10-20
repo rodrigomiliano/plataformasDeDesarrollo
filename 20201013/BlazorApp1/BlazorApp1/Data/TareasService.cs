f591c232515efaa07e338493d03efd4346647354
rodrigo <rodrigomiliano16@gmail.com>
2020-10-19 22:05:21 -0300

    Blazor cambios 19/10

---
+++ 20201013/BlazorApp1/BlazorApp1/Data/TareasService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class TareasService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Tareas[] GetTareas()
+        {
+
+            Tareas[] resultado = new Tareas[10];
+            resultado[0] = new Tareas();
+
+            return resultado;
+        }
+    }
+}
