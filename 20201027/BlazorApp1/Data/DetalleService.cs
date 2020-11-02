43f98bb0c9061e861f4df6bf63a229bd7112e2c8
rodrigo <rodrigomiliano16@gmail.com>
2020-11-02 19:36:57 -0300

    Avances sobre clase 27/10

---
+++ 20201027/BlazorApp1/Data/DetalleService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class DetalleService
+    {
+        private DataContext context;
+
+        public DetalleService(DataContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<Detalle> Get(int id)
+        {
+            return await context.Detalles.Where(i => i.Id == id).SingleAsync();
+        }
+
+        public async Task<List<Detalle>> GetAll()
+        {
+            return await context.Detalles.ToListAsync();
+        }
+
+        public async Task<Detalle> Save(Detalle value)
+        {
+            if (value.Id == 0)
+            {
+                await context.Detalles.AddAsync(value);
+            }
+            else
+            {
+                context.Detalles.Update(value);
+            }
+            await context.SaveChangesAsync();
+            return value;
+        }
+    }
+}
